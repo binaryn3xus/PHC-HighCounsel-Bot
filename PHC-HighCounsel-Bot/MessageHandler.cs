5edc4b62b47ef197f53988484deb8b97303e8f2d
Joshua <Joshua@UNSC-PILLAROFAU>
2024-05-28 00:10:11 -0400

    Add project files.

---
+++ PHC-HighCounsel-Bot/MessageHandler.cs
@@ -0,0 +1,28 @@
+﻿namespace PHC_HighCounsel_Bot;
+
+public class MessageHandler
+{
+    private readonly DiscordSocketClient _client;
+    private readonly InteractionService _commands;
+    private readonly IServiceProvider _services;
+
+    public MessageHandler(DiscordSocketClient client)
+    {
+        _client = client;
+    }
+
+    public async Task InitializeAsync()
+    {
+        _client.MessageReceived += HandleMessageReceived;
+    }
+
+    private async Task HandleMessageReceived(SocketMessage message)
+    {
+        // Check if the message contains "Halo"
+        if (message.Content.Contains("Halo"))
+        {
+            //await message.AddReactionAsync(new Emoji("🎮"));
+            await message.AddReactionAsync(Emote.Parse("<:laughguy:1244830211229618296>"));
+        }
+    }
+}
---
+++ PHC-HighCounsel-Bot/Program.cs
@@ -0,0 +1,39 @@
+﻿Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+Log.Logger = new LoggerConfiguration()
+    .MinimumLevel.Information()
+    .MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+    .WriteTo.Console()
+    .CreateLogger();
+
+var configuration = new ConfigurationBuilder()
+    .AddUserSecrets<Program>()
+    .Build();
+
+var builder = Host.CreateDefaultBuilder(args);
+
+var discordToken = configuration["discord:token"];
+var ollamaServer = configuration["ollama:server"];
+var ollamaModel = configuration["ollama:model"];
+
+builder.ConfigureServices((hostContext, services) =>
+{
+    services.AddSingleton<IConfiguration>(configuration);
+    services.AddSerilog();
+    services.AddSingleton(provider => new DiscordSocketClient(new DiscordSocketConfig() { MessageCacheSize = 200, GatewayIntents = GatewayIntents.All }));
+    services.AddSingleton(x => new InteractionService(x.GetRequiredService<DiscordSocketClient>()));
+    services.AddSingleton<ConversationContext>();
+    services.AddSingleton<CommandHandler>();
+    services.AddSingleton<MessageHandler>();
+    services.AddSingleton(new OllamaApiClient(new Uri(ollamaServer)) { SelectedModel = ollamaModel });
+    services.AddHostedService<DiscordBotService>();
+});
+
+var host = builder.Build();
+
+if(!string.IsNullOrWhiteSpace(ollamaModel))
+{
+    var ollama = host.Services.GetRequiredService<OllamaApiClient>();
+    await ollama.PullModel(ollamaModel, status => Console.WriteLine($"({status.Percent}%) {status.Status}"));
+}
+
+await host.RunAsync();
